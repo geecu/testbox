444420c2e36619a3bcbd629177870e14e2ca7b52
Gunther Konig <geecu.gee@gmail.com>
2018-06-15 14:10:53 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# testbox
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,8 @@
+document.getElementById("app").innerHTML = `
+<h1>Hello my oh my!</h1>
+<div>
+  Look
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
+  for more info about Parcel.
+</div>
+`;
