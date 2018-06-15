5fba4e1cbb67f4023085846a7dd78ce505445b92
Gunther Konig <geecu.gee@gmail.com>
2018-06-15 14:35:43 +0300

    new harcel

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 document.getElementById("app").innerHTML = `
-<h1>Hello my oh my!</h1>
+<h1>Hello my oh my!!</h1>
 <div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
+  Look!
 </div>
 `;
